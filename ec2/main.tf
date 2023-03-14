81222daebc48af70721e610d4fd9164b7f307313
TARAK <tarakaramtirumala@gmail.com>
2023-03-14 08:48:52 +0530

    changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,7 +12,7 @@ resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
 
     connection {
-      host     = aws_instance.ec2.public_ip
+      host     = aws_instance_ec2.public_ip
       user     = "centos"
       password = "DevOps321"
     }
