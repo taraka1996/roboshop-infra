f0cbad5e5481d702440b615528d9e86fdb98f2f4
TARAK <tarakaramtirumala@gmail.com>
2023-03-23 22:57:32 +0530

    vpc

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2" {
-  for_each      = var.instances
-  source        = "./ec2"
-  component     = each.value["name"]
-  instance_type = each.value["type"]
-  env           = var.env
-  monitor       = try(each.value["monitor"], false)
-}
