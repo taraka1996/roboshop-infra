01e344912d296e53f4603b9449a4e9ce047e552b
TARAK <tarakaramtirumala@gmail.com>
2023-03-23 23:06:52 +0530

    changes

--- env-prod/main.tf
+++ env-prod/main.tf
@@ -2,6 +2,6 @@ module "vpc" {
    source = "git::https://gihub.com/taraka1996/tf-module-vpc.git"
    env = var.env
    for_each = var.vpc
-   vpc_cidr = each.value["cidr"]
+   vpc_cidr = each.value["vpc_cidr"]
 }
 
