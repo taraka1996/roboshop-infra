b2c8327b95dde23f03678e25443447451facec59
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 23:26:54 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ module "vpc" {
    source = "git::https://github.com/taraka1996/tf-module-vpc.git"
    env = var.env
    tags = var.tags
+   default_route_table = var.default_route_table
+   default_vpc_id = var.default_vpc_id
 
    for_each = var.vpc
    vpc_cidr = each.value["vpc_cidr"]
