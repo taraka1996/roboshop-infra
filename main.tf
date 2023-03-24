6b339ce8c9bcee64932bb29fd2735d6f358e84cd
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 16:47:26 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
    source = "git::https://github.com/taraka1996/tf-module-vpc.git"
    env = var.env
    tags = var.tags
+   
    for_each = var.vpc
    vpc_cidr = each.value["vpc_cidr"]
    public_subnets = each.value["public_subnets"]
--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
 variable "env" {}
-variable "vpc_cidr" {}
+variable "vpc" {}
 variable "tags" {}
 variable "public_subnets" {}
