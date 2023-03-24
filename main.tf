3891d3307bc8e5f75204203685e14e2a8b9302c1
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 15:23:29 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
    source = "git::https://github.com/taraka1996/tf-module-vpc.git"
    env = var.env
-
+   tags = var.tags
    for_each = var.vpc
    vpc_cidr = each.value["vpc_cidr"]
    
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+tags = {
+   bu-unit_name = "ecommerce"
+   app_name = "roboshop"
+   owner = "ecom_bu"
+   cost_centre = 1001 
+}
