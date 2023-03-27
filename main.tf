085b44b61e8f6df10ed4213d2bce1869cab835a4
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 11:27:29 +0530

    changes

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "docdb" {
   skip_final_snapshot = each.value["skip_final_snapshot"]
   engine_version = each.value["engine_version"]
   instance_class = each.value["instance_class"]
-   kms_key_id = data.aws_kms_key.key.arn
+  kms_key_id = data.aws_kms_key.key.arn
   
  
 }
