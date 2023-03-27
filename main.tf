6f70a17212c0b094787b21d7b439fbf7a1eb3f2a
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 10:38:30 +0530

    changes

--- main.tf
+++ main.tf
@@ -26,6 +26,8 @@ module "docdb" {
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot = each.value["skip_final_snapshot"]
   engine_version = each.value["engine_version"]
+  instance_class = each.value["instance-class"]
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
  
 }
 
