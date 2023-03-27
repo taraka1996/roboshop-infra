b218da7b701989cd7a6caccec7d2260f03ae2907
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 07:33:53 +0530

    changes

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ module "docdb" {
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot = each.value["skip_final_snapshot"]
   engine_version = each.value["engine_version"]
-  subnet_ids = local.db_subnet_ids
+  
 }
 
