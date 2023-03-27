85630fa50c385957705d2e924f1d8ddc7f48a425
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 11:19:06 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   ## Private Subnets
   #private_subnet_ids = { for k, v in module.vpc["main"].private_subnets : k => tomap({ "id" = v.id, "availability_zone" = v.availability_zone }) }
   #private_subnet_ids = [for k, v in module.vpc["main"].private_subnets : v.id]
-  subnet_ids = {
+  db_subnet_ids = {
     db     = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
     app    = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
     web    = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
