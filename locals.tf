b054cebfe7f532d691212aece28ea6609168b607
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 08:31:13 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-    subnet_ids = {
+  subnet_ids = {
     db     = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
     app    = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
     web    = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
