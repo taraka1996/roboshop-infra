757af1e8c220bf5ed68f6ddeb009584389db31fc
TARAK <tarakaramtirumala@gmail.com>
2023-03-29 11:21:32 +0530

    changes

--- main.tf
+++ main.tf
@@ -106,8 +106,8 @@ module "app" {
   max_size = each.value["max_size"]
   min_size = each.value["min_size"]
   desired_capacity = each.value["desired_capacity"]
-  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
+  subnets = lookup(local.subnet_cidr, each.value["subnet_name"], null)
   port              = each.value["port"]
-  allow_app_to      = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
+  allow_app_to      = lookup(local.subnet_ids, each.value["allow_app_to"], null)
   
 }
