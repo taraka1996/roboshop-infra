c56e74550a73dc466ae6f1d914379ce284a4e0e5
TARAK <tarakaramtirumala@gmail.com>
2023-03-30 12:30:00 +0530

    changes

--- main.tf
+++ main.tf
@@ -115,6 +115,6 @@ module "app" {
   subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
   port              = each.value["port"]
   allow_app_to      = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
-  alb_dns_name = lookup(lookup(module.alb, each.value["alb"], null), "dns_name" , null)
+  alb_dns_name = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb" , null), "dns_name", null)
 
 }
