75a46f1bb9dbbc76d227bfd5ace046634124a00b
TARAK <tarakaramtirumala@gmail.com>
2023-04-02 22:35:45 +0530

    changes

--- main.tf
+++ main.tf
@@ -133,3 +133,40 @@ module "app" {
   listener_arn      = lookup(lookup(lookup(module.alb, each.value["alb"], null), "listener", null), "arn", null)
   parameters        = each.value["parameters"]
 }
+
+
+### Load Runner
+
+resource "aws_spot_instance_request" "laod-runner" {
+  ami                  = data.aws_ami.ami.id
+  instance_type        = "t3.medium"
+  subnets = lookup(local.subnet_ids, "public", null)[0]
+  vpc_security_group_ids = [aws_security_group.loadrunner.id]
+  iam_instance_profile   = aws_iam_instance_profile.main.name
+  
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-load-runner" }
+  )
+}
+
+resource "aws_security_group" "laod-runner" {
+  name        = "load-runner"
+  description = "load-runner"
+  vpc_id      = module.vpc["main"].vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
