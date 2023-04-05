2ab8b237d2d8e701934764d07265c9270ea12229
TARAK <tarakaramtirumala@gmail.com>
2023-04-05 08:19:40 +0530

    changes

--- data.tf
+++ data.tf
@@ -6,7 +6,7 @@ data "aws_ssm_parameter" "ssh_pass" {
 
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "devops-practice-with-ansible"
+  name_regex  = "devops-practice with ansible"
   owners      = ["self"]
 
 }
